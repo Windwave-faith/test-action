67d064464c9f939bf100ec11dbb219c0482d7032
Kaznaami for Private <50604480+Windwave-faith@users.noreply.github.com>
2020-07-22 17:19:10 +0900

    Update EntryPoint.ps1

--- EntryPoint.ps1
+++ EntryPoint.ps1
@@ -1,3 +1,3 @@
 @('buckets', 'cache') | ForEach-Object { New-Item (Join-Path $env:SCOOP $_) -Force -ItemType Directory | Out-Null }
 Get-ChildItem (Join-Path $env:SCOOP_HOME 'lib') '*.ps1' | ForEach-Object { . $_.FullName }
-. "${env:SCOOP_HOME}/bin/scoop.ps1" help
+. "./bin/checkver.ps1" -U
